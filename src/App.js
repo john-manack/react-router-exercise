7950df5bcee8f778b175e41ae4136e3f6d6aba63
John Manack <jmnck28@gmail.com>
2021-03-02 15:32:00 -0500

    initial commit, README, basic file structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import Home from './components/Home';
+import Blog from './components/Blog';
+import About from './components/About';
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <Link to='/'>Home</Link>
+          <Link to='/blog'>Blog</Link>
+          <Link to='/about'>About</Link>
+        </nav>
+      </div>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/blog'>
+          <Blog />
+        </Route>
+        <Route path='/about'>
+          <About />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
